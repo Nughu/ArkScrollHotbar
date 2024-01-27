1e64ad0b6b9d46caaaee6641ea6a833ed4b8cbf0
BuildTools <unconfigured@null.spigotmc.org>
2024-01-28 00:36:28 +0100

    added no-unequip function (lastpos variable)

--- main.py
+++ main.py
@@ -3,34 +3,39 @@ import time
 import pynput
 
 # Variables
-min_pos = 1 # define from which hotbar slot the scroll wheel should work
-max_pos = 10 # define up to which hotbar slot the scroll wheel should work
+min_pos = 1  # define from which hotbar slot the scroll wheel should work
+max_pos = 7  # define up to which hotbar slot the scroll wheel should work
 
 # Main Process
 pos = 1
 controller = pynput.keyboard.Controller()
+lastpos = 1
+
 
 def on_scroll(x, y, dx, dy):
-    dir = 'down' if dy < 0 else 'up'
+    direc = 'down' if dy < 0 else 'up'
     global pos
-    if dir == "down":
+    global lastpos
+    if direc == "down":
         if pos != min_pos:
             pos -= 1
-        controller.press((pynput.keyboard.KeyCode(pos + 48)))
-        #time.sleep(0.25)
-        controller.release((pynput.keyboard.KeyCode(pos + 48)))
-    if dir == "up":
+        if pos != lastpos:
+            controller.press((pynput.keyboard.KeyCode(pos + 48)))
+            # time.sleep(0.25)
+            controller.release((pynput.keyboard.KeyCode(pos + 48)))
+    if direc == "up":
         if pos != max_pos:
             pos += 1
-        if pos == 10:
-            controller.press(pynput.keyboard.KeyCode(48))
-            #time.sleep(0.25)
-            controller.release(pynput.keyboard.KeyCode(48))
-        if pos != 10:
-            controller.press((pynput.keyboard.KeyCode(pos + 48)))
-            #time.sleep(0.25)
-            controller.release((pynput.keyboard.KeyCode(pos + 48)))
-
+        if pos != lastpos:
+            if pos == 10:
+                controller.press(pynput.keyboard.KeyCode(48))
+                # time.sleep(0.25)
+                controller.release(pynput.keyboard.KeyCode(48))
+            if pos != 10:
+                controller.press((pynput.keyboard.KeyCode(pos + 48)))
+                # time.sleep(0.25)
+                controller.release((pynput.keyboard.KeyCode(pos + 48)))
+    lastpos = pos
 
 
 # ...or, in a non-blocking fashion:
